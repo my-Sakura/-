5327ce474fe1b871d225bddb0b07f3df9b188295
lizebang <lizebang@lizebangold.local>
2020-11-08 09:32:31 +0800

    chatroom

---
+++ client/client.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+
+	conn, err := net.Dial("tcp", "192.168.0.101:1010")
+	if err != nil {
+		fmt.Println("dialErr: ", err)
+		os.Exit(0)
+	}
+	sendMessage(conn)
+
+}
+
+func sendMessage(conn net.Conn) {
+	userName := conn.LocalAddr().String()
+	var msg string
+
+	go receive(conn)
+
+	for {
+		fmt.Scanln(&msg)
+
+		if msg == "quit" {
+			break
+		}
+		_, err := conn.Write([]byte(userName + ":" + msg))
+		if err != nil {
+			fmt.Println("messageSendErr: ", err)
+			conn.Close()
+			break
+		}
+	}
+}
+
+func receive(conn net.Conn) {
+	msg := make([]byte, 1024)
+
+	for {
+		_, err := conn.Read(msg)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(string(msg))
+	}
+}
